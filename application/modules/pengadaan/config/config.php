45bc0a4b2c91e54a480830e1e1abe7c041bf8edd
Faytlla4 <Faytlla4@users.noreply.github.com>
2026-09-22 13:46:08 +0700

    fix: sidebar pengadaan apotek jadi link langsung tanpa dropdown 1 anak

--- application/modules/pengadaan/config/config.php
+++ application/modules/pengadaan/config/config.php
@@ -1,2 +1,4 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
-$config['module_config'] = array('description' => 'Pengadaan Obat', 'name' => 'Pengadaan', 'version' => '1.0.0', 'author' => 'Klinik Apotek Team', 'menus' => array('apotek' => 'pengadaan/_menu_apotek', 'master' => 'pengadaan/_menu_master'));
+// ponytail: tanpa menus.apotek -> Contextslte render link langsung
+// (bukan dropdown parent dgn 1 anak).
+$config['module_config'] = array('description' => 'Pengadaan Obat', 'name' => 'Pengadaan', 'version' => '1.0.0', 'author' => 'Klinik Apotek Team', 'menus' => array('master' => 'pengadaan/_menu_master'));
--- application/modules/pengadaan/views/_menu_apotek.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li class="nav-item">
-        <a href="<?php echo site_url(SITE_AREA . '/apotek/pengadaan'); ?>" class="nav-link <?php echo $this->uri->segment(2) == 'apotek' && $this->uri->segment(3) == 'pengadaan' ? 'active' : ''; ?>">
-            <i class="far fa-circle nav-icon"></i>
-            <p>Pengadaan Obat</p>
-        </a>
-    </li>
-</ul>
